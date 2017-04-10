e84a5cc159fa9f0cdde844b497f0c54daf3b803d
gunhappy <gunhappy123@gmail.com>
2017-04-10 20:04:11 +0700

    first commit

---
+++ src/sample/spring/BankAccountDetails.java
@@ -0,0 +1,43 @@
+package sample.spring; /**
+ * Created by Phasin Sarunpornkul on 4/3/2017 AD.
+ */
+import java.util.Date;
+
+public class BankAccountDetails {
+    private int accountId;
+    private int balanceAmount;
+    private Date lastTransactionTimestamp;
+    private final String signature = "5710546356";
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public int getBalanceAmount() {
+        return balanceAmount;
+    }
+
+    public void setBalanceAmount(int balanceAmount) {
+        this.balanceAmount = balanceAmount;
+    }
+
+    public Date getLastTransactionTimestamp() {
+        return lastTransactionTimestamp;
+    }
+
+    public void setLastTransactionTimestamp(Date lastTransactionTimestamp) {
+        this.lastTransactionTimestamp = lastTransactionTimestamp;
+    }
+
+    @Override
+    public String toString() {
+        return signature + " sample.spring.BankAccountDetails [accountId=" + accountId
+                + ", balanceAmount=" + balanceAmount
+                + ", lastTransactionTimestamp=" + lastTransactionTimestamp
+                + "]";
+    }
+}
---
+++ src/sample/spring/BankAccountService.java
@@ -0,0 +1,8 @@
+package sample.spring;
+
+/**
+ * Created by Phasin Sarunpornkul on 4/3/2017 AD.
+ */
+public interface BankAccountService {
+    int createBankAccount(BankAccountDetails bankAccountDetails);
+}
